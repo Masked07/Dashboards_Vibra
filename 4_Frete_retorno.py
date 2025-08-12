7e72a9173a672ca595d49d702774aea1e017f094
Masked07 <139254826+Masked07@users.noreply.github.com>
2025-08-12 10:24:45 -0300

    Update 4_Frete_retorno.py

--- 4_Frete_retorno.py
+++ 4_Frete_retorno.py
@@ -31,7 +31,7 @@ def carregar_dados():
         np.floor((df['Saida do Cliente'] - df['ETA Chegada Cliente']).dt.total_seconds() / 3600)
         .fillna(0).astype(int))
 
-    df['SLA 2'] = df['SLA 2'].replace({'Dentro SLA': 'Sim', 'Fora SLA': 'Não'})
+    df['SLA 2'] = df['SLA 2'].replace({'DENTRO SLA': 'Sim', 'FORA SLA': 'Não'})
 
     return df
 
@@ -202,4 +202,5 @@ fig_linha.update_traces(line_color='#008000',
                         textposition='top center')
 
 st.plotly_chart(fig_linha, use_container_width=True)
+
 
